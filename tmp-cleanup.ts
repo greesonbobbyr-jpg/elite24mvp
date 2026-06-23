299a184326d88350fd0deeab40a8ee58d6008121
Bobby Greeson <256476846+greesonbobbyr-jpg@users.noreply.github.com>
2026-06-23 12:39:40 -0500

    Remove temporary test files from GIF build

--- tmp-cleanup.ts
+++
@@ -1,6 +0,0 @@
-﻿import { prisma } from "./lib/prisma";
-(async () => {
-  const r = await prisma.teamMessage.deleteMany({ where: { gifId: "__tmp_test" } });
-  console.log("deleted_temp_msgs=" + r.count);
-  await prisma.$disconnect();
-})();
